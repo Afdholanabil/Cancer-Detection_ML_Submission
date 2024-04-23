44e18cab423495a1e1c563819a1a1e307085193d
Afdholanabil <afdholanabil41435@gmail.com>
2024-04-23 21:01:50 +0700

    adding UCrop and solved a problem with tensorflow depedency

--- app/src/main/java/com/example/submission_mlforandroid/helper/ImageClassifierHelper.kt
+++ app/src/main/java/com/example/submission_mlforandroid/helper/ImageClassifierHelper.kt
@@ -1,11 +1,19 @@
 package com.example.submission_mlforandroid.helper
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.SystemClock
+import android.util.Log
+import com.example.submission_mlforandroid.R
+
+import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.task.core.BaseOptions
 import org.tensorflow.lite.task.vision.classifier.Classifications
 import org.tensorflow.lite.task.vision.classifier.ImageClassifier
-import java.lang.Error
+import java.io.FileNotFoundException
+import java.lang.IllegalStateException
 
 class ImageClassifierHelper(
     var threshold : Float = 0.1f,
@@ -15,6 +23,7 @@ class ImageClassifierHelper(
     val classifierListener: ClassifierListener?
 ) {
 
+    private var imageClassifier: ImageClassifier? = null
     init {
         setupImageClassifier()
     }
@@ -34,10 +43,55 @@ class ImageClassifierHelper(
         val baseOptionBuilder = BaseOptions.builder()
             .setNumThreads(4)
         optionBuilder.setBaseOptions(baseOptionBuilder.build())
+
+        try {
+            imageClassifier = ImageClassifier.createFromFileAndOptions(
+                context,
+                modelName,
+                optionBuilder.build()
+            )
+        } catch (e : IllegalStateException) {
+            classifierListener?.onError(context.getString(R.string.image_classifier_failed))
+            Log.e(TAG, e.message.toString())
+        }
     }
 
     fun classifyStaticImage(imageUri: Uri) {
-        // TODO: mengklasifikasikan imageUri dari gambar statis.
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        val bitmap = getBitmapFromUri(imageUri)
+        if (bitmap != null) {
+            val resizedBitmap = resizeBitmap(bitmap, 224, 224)
+            val tensorImage = TensorImage(org.tensorflow.lite.DataType.FLOAT32)
+            tensorImage.load(resizedBitmap)
+
+            var inferenceTime = SystemClock.uptimeMillis()
+            val results = imageClassifier?.classify(tensorImage)
+            inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+            classifierListener?.onResult(results, inferenceTime)
+        } else {
+            classifierListener?.onError("Failed to load bitmap from Uri")
+        }
     }
 
+    private fun getBitmapFromUri(uri: Uri): Bitmap? {
+        return try {
+            val inputStream = context.contentResolver.openInputStream(uri)
+            BitmapFactory.decodeStream(inputStream)
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+            null
+        }
+
+    }
+
+    private fun resizeBitmap(bitmap: Bitmap, width: Int, height: Int): Bitmap {
+        return Bitmap.createScaledBitmap(bitmap, width, height, true)
+    }
+
+    companion object {
+        private const val TAG = "ImageClassifierHelper"
+    }
 }
--- app/src/main/java/com/example/submission_mlforandroid/view/MainActivity.kt
+++ app/src/main/java/com/example/submission_mlforandroid/view/MainActivity.kt
@@ -1,12 +1,95 @@
 package com.example.submission_mlforandroid.view
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.submission_mlforandroid.R
+import com.example.submission_mlforandroid.databinding.ActivityMainBinding
+import com.yalantis.ucrop.UCrop
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private var currentImageUri: Uri? = null
+    private var afterUCropImageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.analyzeButton.setOnClickListener { analyzeImage() }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+            startUCrop()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun startUCrop() {
+        val destinationFileName = "cropped_image.jpg"
+        val uCrop = UCrop.of(currentImageUri!!, Uri.fromFile(File(cacheDir, destinationFileName)))
+        uCrop.start(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            val resultUri = UCrop.getOutput(data!!)
+            if (resultUri != null) {
+                afterUCropImageUri = resultUri
+                showImage()
+            } else {
+                showToast("Failed to crop image")
+            }
+        }
+    }
+
+    private fun showImage() {
+        afterUCropImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
+    }
+
+    private fun analyzeImage() {
+        showToast("Analyze")
+    }
+
+    private fun moveToResult() {
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
     }
 }
